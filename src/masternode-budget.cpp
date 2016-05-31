f57183a8bda4e9d99050d1b0f65334f2844e31e1
Evan Duffield <evan@dashpay.io>
2016-05-31 11:27:11 -0700

    Remove nTime from IsCollateralValid and document function

--- src/masternode-budget.cpp
+++ src/masternode-budget.cpp
@@ -164,7 +164,7 @@ void CBudgetManager::ProcessMessage(CNode* pfrom, std::string& strCommand, CData
 
         std::string strError = "";
         int nConf = 0;
-        if(!IsCollateralValid(finalizedBudgetBroadcast.nFeeTXHash, finalizedBudgetBroadcast.GetHash(), strError, finalizedBudgetBroadcast.nTime, nConf, GOVERNANCE_FEE_TX)){
+        if(!IsCollateralValid(finalizedBudgetBroadcast.nFeeTXHash, finalizedBudgetBroadcast.GetHash(), strError, nConf, GOVERNANCE_FEE_TX)){
             LogPrintf("Finalized Budget FeeTX is not valid - %s - %s\n", finalizedBudgetBroadcast.nFeeTXHash.ToString(), strError);
 
             if(nConf >= 1) vecImmatureFinalizedBudgets.push_back(finalizedBudgetBroadcast);
@@ -254,7 +254,7 @@ void CBudgetManager::NewBlock()
     {
         std::string strError = "";
         int nConf = 0;
-        if(!IsCollateralValid((*it5).nFeeTXHash, (*it5).GetHash(), strError, (*it5).nTime, nConf, GOVERNANCE_FEE_TX)){
+        if(!IsCollateralValid((*it5).nFeeTXHash, (*it5).GetHash(), strError, nConf, GOVERNANCE_FEE_TX)){
             ++it5;
             continue;
         }
@@ -835,7 +835,7 @@ bool CFinalizedBudget::IsValid(const CBlockIndex* pindex, std::string& strError,
     std::string strError2 = "";
     if(fCheckCollateral){
         int nConf = 0;
-        if(!IsCollateralValid(nFeeTXHash, GetHash(), strError2, nTime, nConf, GOVERNANCE_FEE_TX)){
+        if(!IsCollateralValid(nFeeTXHash, GetHash(), strError2, nConf, GOVERNANCE_FEE_TX)){
             strError = "Invalid Collateral : " + strError2;
             return false;
         }
