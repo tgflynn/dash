7bdaf0a49767139d507566819dcd39c56da0ea43
Tim Flynn <tgflynn@cogniception.com>
2016-08-10 14:11:34 -0400

    Removed some unwrapped debugging printf's

--- src/masternode-payments.cpp
+++ src/masternode-payments.cpp
@@ -102,8 +102,6 @@ bool IsBlockPayeeValid(const CTransaction& txNew, int nBlockHeight)
 
     // SEE IF THIS IS A VALID SUPERBLOCK
 
-    printf("IsBlockPayeeValid 1");
-
     if(CSuperblockManager::IsValidSuperblockHeight(nBlockHeight))
     {
         if(CSuperblockManager::IsSuperblockTriggered(nBlockHeight))
@@ -111,21 +109,17 @@ bool IsBlockPayeeValid(const CTransaction& txNew, int nBlockHeight)
             // IF WE HAVE A ACTIVATED TRIGGER
 
             if(CSuperblockManager::IsValid(txNew, nBlockHeight)){
-                printf("2\n");
                 return true;
             } else {
                 LogPrintf("Invalid superblock detected %s\n", txNew.ToString());
                 if(sporkManager.IsSporkActive(SPORK_9_MASTERNODE_SUPERBLOCK_ENFORCEMENT)) {
-                    printf("3\n");
                     return false;
                 } else {
-                    printf("4\n");
                     LogPrintf("Superblock enforcement is disabled, accepting block\n");
                     return true;
                 }
             }
 
-            printf("5\n");
             return false; //note, is this correct?
         }
     }
@@ -164,17 +158,13 @@ void FillBlockPayee(CMutableTransaction& txNew, CAmount nFees)
     // SEE IF THIS IS A VALID SUPERBLOCK
 
 
-    printf("FillBlockPayee 1");
-
     if(CSuperblockManager::IsValidSuperblockHeight(nHeight))
     {
-        printf("2");
         if(CSuperblockManager::IsSuperblockTriggered(nHeight))
         {
             // IF WE HAVE A ACTIVATED TRIGGER
             LogPrint("gobject", "FillBlockPayee, triggered superblock creation @ height %d\n", nHeight);
             CSuperblockManager::CreateSuperblock(txNew, nFees, nHeight);
-            printf("3\n");
             return;
         }
     }
@@ -182,7 +172,6 @@ void FillBlockPayee(CMutableTransaction& txNew, CAmount nFees)
     // FILL BLOCK PAYEE WITH MASTERNODE PAYMENT OTHERWISE
 
     mnpayments.FillBlockPayee(txNew, nFees);
-    printf("5\n");
 }
 
 std::string GetRequiredPaymentsString(int nBlockHeight)
