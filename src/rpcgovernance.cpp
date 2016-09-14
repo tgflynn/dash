a6d945a2823fef9a5d1e6e0a43199433fdb5ce00
Tim Flynn <tgflynn@users.noreply.github.com>
2016-09-14 19:25:31 -0400

    Improve rpcgovernance error handling  - Prevent attempts to prepare trigger objects (and waste the collateral)  - Improve clarity of gobject submit error messages

--- src/rpcgovernance.cpp
+++ src/rpcgovernance.cpp
@@ -76,8 +76,9 @@ UniValue gobject(const UniValue& params, bool fHelp)
     // PREPARE THE GOVERNANCE OBJECT BY CREATING A COLLATERAL TRANSACTION
     if(strCommand == "prepare")
     {
-        if (params.size() != 6)
+        if (params.size() != 6) {
             throw JSONRPCError(RPC_INVALID_PARAMETER, "Correct usage is 'gobject prepare <parent-hash> <revision> <time> <name> <data-hex>'");
+        }
 
         // ASSEMBLE NEW GOVERNANCE OBJECT FROM USER PARAMETERS
 
@@ -106,6 +107,10 @@ UniValue gobject(const UniValue& params, bool fHelp)
         // CREATE A NEW COLLATERAL TRANSACTION FOR THIS SPECIFIC OBJECT
 
         CGovernanceObject govobj(hashParent, nRevision, strName, nTime, uint256(), strData);
+
+        if(govobj.GetObjectType() == GOVERNANCE_OBJECT_TRIGGER) {
+            throw JSONRPCError(RPC_INVALID_PARAMETER, "Trigger objects need not be prepared (however only masternodes can create them)");
+        }
 
         std::string strError = "";
         if(!govobj.IsValidLocally(pindex, strError, false))
@@ -149,10 +154,6 @@ UniValue gobject(const UniValue& params, bool fHelp)
              << ", params.size() = " << params.size()
              << ", mnFound = " << mnFound << endl; );
 
-        if((params.size() == 6) && (!mnFound))  {
-            throw JSONRPCError(RPC_CLIENT_IN_INITIAL_DOWNLOAD, "Non-masternodes must include fee-txid parameter.");
-        }
-
         // ASSEMBLE NEW GOVERNANCE OBJECT FROM USER PARAMETERS
 
         LOCK(cs_main);
@@ -194,7 +195,12 @@ UniValue gobject(const UniValue& params, bool fHelp)
                 govobj.Sign(activeMasternode.keyMasternode, activeMasternode.pubKeyMasternode);
             }
             else {
-                throw JSONRPCError(RPC_INTERNAL_ERROR, "Only valid masternodes can submit this type of object");
+                throw JSONRPCError(RPC_INVALID_PARAMETER, "Only valid masternodes can submit this type of object");
+            }
+        }
+        else {
+            if(params.size() != 7) {
+                throw JSONRPCError(RPC_INVALID_PARAMETER, "The fee-txid parameter must be included to submit this type of object");
             }
         }
 
@@ -209,7 +215,6 @@ UniValue gobject(const UniValue& params, bool fHelp)
         governance.AddGovernanceObject(govobj);
 
         return govobj.GetHash().ToString();
-
     }
 
     if(strCommand == "vote-conf")
