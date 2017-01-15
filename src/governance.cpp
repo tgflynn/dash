5934b1a9bd27410ca705e60136ac92d80bde3b2d
Tim Flynn <tgflynn@users.noreply.github.com>
2017-01-15 16:34:07 -0500

    Update sync time for inv's

--- src/governance.cpp
+++ src/governance.cpp
@@ -650,6 +650,9 @@ bool CGovernanceManager::ConfirmInventoryRequest(const CInv& inv)
         LogPrint("gobject", "CGovernanceManager::ConfirmInventoryRequest added inv to requested set\n");
     }
 
+    // Keep sync alive
+    masternodeSync.AddedGovernanceItem();
+
     LogPrint("gobject", "CGovernanceManager::ConfirmInventoryRequest reached end, returning true\n");
     return true;
 }
